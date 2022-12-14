783c1d1cfb8069fe6cb327807c3d01a9277a4022
Prabal Pathak <prabal01pathak@gmail.com>
2022-12-14 09:35:19 +0000

    added initial main.rs file

--- rust-prac/src/main.rs
+++ rust-prac/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
+    // first print line
     println!("Hello, world!");
+    /* second print line */
+    println!("I'm a Rustacean!");
+    let x = 5 + /* 90 + */ 5;
+    println!("Is `x` 10 or 100? x = {}", x);
 }
