d60f8a7cab66a76a0b0edab6e7682850ee2a11a4
Prabal Pathak <prabal01pathak@gmail.com>
2022-12-18 11:53:28 +0000

    update main.rs

--- rust-prac/src/main.rs
+++ rust-prac/src/main.rs
@@ -1,8 +1,7 @@
+// #[warn(unused_imports)]
+#[allow(unused)]
+use std::cmp::Ordering;
+
 fn main() {
-    // first print line
-    println!("Hello, world!");
-    /* second print line */
-    println!("I'm a Rustacean!");
-    let x = 5 + /* 90 + */ 5;
-    println!("Is `x` 10 or 100? x = {}", x);
+    let operators = "+-*/%";
 }
