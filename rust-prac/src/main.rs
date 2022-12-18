1d94995cd2ff002ecbb1ca498ced7ee7d04f5189
Prabal Pathak <prabal01pathak@gmail.com>
2022-12-18 15:27:26 +0000

    added guessing game

--- rust-prac/src/main.rs
+++ rust-prac/src/main.rs
@@ -1,7 +1,34 @@
-// #[warn(unused_imports)]
-#[allow(unused)]
+// guessing game
+
+use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
-    let operators = "+-*/%";
+    println!("Guess the number.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {
+                println!("You Win!");
+                break;
+            }
+        }
+    }
 }
